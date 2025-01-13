c52d6e32ec2364ed8bcae8526352796d29b75625
Matteo Leccese <matteoleccese2099@gmail.com>
2025-01-13 16:10:08 -0400

    fixing a bug, and updating the project components, and finishing the error page

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,41 +1,52 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { RepositorySearchState, RootStoreState } from '../interfaces/store.interfaces';
 import { fetchGithubCommits } from '../store/commitHistorySlice';
 import { AppDispatch } from '../store/store';
+import { FormEvent, useCallback } from 'react';
 
-const SearchBar = () => {
+export const SearchBar = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { user, repo } = useSelector((state: RootStoreState) => state.repositorySearch);
 
-  const { register, handleSubmit } = useForm<RepositorySearchState>({
+  const { register } = useForm<RepositorySearchState>({
     defaultValues: {
       user,
       repo,
     },
   })
 
-  const onSubmit: SubmitHandler<RepositorySearchState> = ({user, repo}: RepositorySearchState) => dispatch(fetchGithubCommits(user, repo));
+  const handleOnSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const formObject: RepositorySearchState = Object.fromEntries(formData.entries()) as unknown as RepositorySearchState;
+    dispatch(fetchGithubCommits(formObject.user, formObject.repo));
+  }, [ dispatch ]);
 
   return (
-    <>
-      <form className="w-full flex align-middle justify-around"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <input className="w-2/5 border-2 border-blue-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" placeholder="GitHub user here"
-          required
-          type="text"
-          {...register("user", { required: true, maxLength: 20, max: 20 })}
-        />
-        <input className="w-2/5 border-2 border-blue-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" placeholder="GitHub repository here"
-          required
-          type="text"
-          {...register("repo", { required: true, maxLength: 20, max: 20 })}
-        />
-        <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" value="Send" />
-      </form>
-    </>
+    <form
+      className="w-full flex flex-row align-middle justify-around"
+      onSubmit={(e) => handleOnSubmit(e)}
+    >
+      <input
+        className="w-2/5 border-2 border-blue-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+        placeholder="GitHub user here"
+        required
+        type="text"
+        {...register("user", { required: true, maxLength: 20, max: 20 })}
+      />
+      <input
+        className="w-2/5 border-2 border-blue-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+        placeholder="GitHub repository here"
+        required
+        type="text"
+        {...register("repo", { required: true, maxLength: 20, max: 20 })}
+      />
+      <input
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="submit"
+        value="Search"
+      />
+    </form>
   )
-}
-
-export default SearchBar
+};
--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,38 +1,31 @@
-import { FunctionComponent, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchGithubCommits } from "../store/commitHistorySlice";
-import { AppDispatch } from "../store/store";
+import { FunctionComponent } from "react";
+import { useSelector } from "react-redux";
 import { CommitHistory } from "../interfaces/github.interfaces";
 import { RootStoreState } from "../interfaces/store.interfaces";
 import CommitEntry from "../components/CommitEntry";
-import SearchBar from "../components/SearchBar";
+import { SearchBar } from "../components/SearchBar";
 
-const Index: FunctionComponent = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const { user, repo } = useSelector((state: RootStoreState) => state.repositorySearch);
-  const commitsArray: CommitHistory[] = useSelector((state: RootStoreState) => state.commitHistory.commitHistory);
-
-  useEffect(() => {
-    return () => {
-      if (commitsArray.length === 0) {
-        dispatch(fetchGithubCommits(user, repo));
-      }
-    } 
-  }, [user, repo, dispatch, commitsArray])
+export const Index: FunctionComponent = () => {
+  const { hasErrors, commitHistory } = useSelector((state: RootStoreState) => state.commitHistory);
 
   return (
-    <div className="w-screen h-screen p-10 flex flex-col gap-2 items-center">
-      <h1 className="text-3xl font-bold tracking-normal">FullTimeForce - GitHub Repository Search</h1>
+    <div className="w-[100%] h-screen p-10 flex flex-col gap-2 items-center">
+      <h1 className="text-3xl font-bold tracking-normal">Github Commit Tracker</h1>
       <SearchBar />
-      <>
-        { 
-          commitsArray && commitsArray.length ? (
-            commitsArray.map((commitHistory: CommitHistory) => <CommitEntry key={commitHistory.node_id} commitHistory={commitHistory} />) 
-          ) : <p>Nothing was found</p>
-        }
-      </>
+      { 
+        commitHistory && Array.isArray(commitHistory) && commitHistory.length !== 0 ? (
+          commitHistory.map((commitHistory: CommitHistory) => <CommitEntry key={commitHistory.node_id} commitHistory={commitHistory} />) 
+        ) : (
+          <div className="w-[100%] h-auto m-auto p-2 flex flex-col gap-2 items-center">
+            <img
+              className="w-[250px] m-auto"
+              src={hasErrors ? "/error.png" : "/search.png"}
+              alt="search"
+            />
+            <p>{hasErrors ? "The project does not exist" : "Search a project!"}</p>
+          </div>
+        )
+      }
     </div>
-  )
-}
-
-export default Index;
+  );
+};
